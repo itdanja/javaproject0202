0621490629a654fb949354554cd4ed3557f81c61
505-t <itdanja@naver.com>
2023-02-02 21:37:05 +0900

    day01

--- src/Day01/Ex1.java
+++ src/Day01/Ex1.java
@@ -8,8 +8,46 @@
     여러줄 주석
 */
 
-package Day01;
 
-public class Ex1 {
+package Day01; // 현재 클래스(소스)파일이 위치한 패키지명 표시
 
-}
+// !: 자바는 무조건 클래스 안 {  }에서 코드 작성
+// int a ; // 빨간줄 나오면서 오류 라서 주석
+public class Ex1 { // 클래스 시작
+    int b ; // 빨간줄 없으면서 오류없다. // 화면 출력 X
+    // 실행해야지 --> 실행화살표 없다
+    // m + 엔터
+    // 실행할 코드는 무조건 main함수 { } 안에서 작성
+    // main함수 : main스레드(코드 실제로 실행하는 주체)
+    public static void main(String[] args) { // main 시작
+        // 콘솔창에 출력
+        // sout + 엔터
+        // System.out.println("출력하고 싶은말");
+        System.out.println("안녕하세요 자바!!");
+        System.out.println("두번째 출력입니다.");
+
+        // 제어문자[이스케이프문자]    \ : 엔터위에 원화기호
+        System.out.println("문자열 출력");               // "문자열" 구분
+        System.out.println("안녕하세요\t들여쓰기");      // \t : 들여쓰기
+        System.out.println("안녕하세요\n줄바꿈");       // \n : 줄바꿈
+        //System.out.println(" "안녕하세요" ");       // " 출력x
+        System.out.println(" \"안녕하세요\" ");      // \" 출력o
+        //System.out.println(" \ ");                // \ 출력x
+        System.out.println(" \\ ");                 // \ 출력o
+
+        // 예제1 : 토끼 출력하자
+        /*
+            \    /\
+             )  ( ')
+            (  /  )
+             \(__)|
+         */
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( \')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|"); // | : 엔터위에 원화기호 + shift
+
+
+    } // main 끝
+} // 클래스 끝
+
