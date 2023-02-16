dc70bab0461022d9fe38e2b0559ab77b340104ec
505-t <itdanja@naver.com>
2023-02-16 20:21:20 +0900

    day05 예금

--- src/Day05/P1_예금프로그램.java
+++ src/Day05/P1_예금프로그램.java
@@ -4,7 +4,7 @@
         사용되는문법 : 출력,입력,변수,연산,제어,반복
         조건
             1. 기능 메뉴 : 예금 , 출금 , 잔고확인
-            2. 통장 : 1개[ 계좌번호 : 20221227 , 비밀번호 : 1234 , 초기잔금 : 0 ]
+            2. 통장 : 1개[ 계좌번호 : 20230216 , 비밀번호 : 1234 , 초기잔금 : 0 ]
  */
 package Day05; // 현재 클래스파일가 위치한 패키지[폴더]명 명시
 
@@ -16,24 +16,50 @@ public class P1_예금프로그램 { // class start // 자바 코드는 무조�
     // ----------------------------- main -------------------
     public static void main(String[] args) { // main end
         // 자바 코드를 읽어주는 main함수(+쓰레드)[ 실행할 코드는 main { } 안에서 작성한다.]
-        while (true){ // while start // 무한루프 [ 종료조건 : 4 입력시 ]
+
+        // * 통장 데이터 변수 [ while 밖에 만드는 이유 : 통장 관련 변수들은 선언 1번 ]
+        int account = 20230216; // -- 계좌번호 변수
+        int password = 1234;    // -- 비밀번호 변수
+        int money  = 0 ;        // -- 통장 잔금 변수
+
+        while (true){ // while start // 무한루프 [ 종료조건 : 4 입력시 - break; 반복문종료 ]
             System.out.println(" -------------- 이젠 은행 -------------- ");
             System.out.print("1.예금 2.출금 3.잔고 4.종료 : ");
             // 1. 입력받기
-            Scanner scanner = new Scanner(System.in);
-            int menuChoice = scanner.nextInt();
+            Scanner scanner = new Scanner(System.in);   // 1. 입력객체
+            int menuChoice = scanner.nextInt();         // 2. 입력객체로 부터 입력받은 값을 정수형으로 변환후 가져와서 menuChoice 에 저장
             // 2. 선택한 메뉴의 경우의수
-            if( menuChoice == 1 ){
+            // ---------------------------------- 1. 예금 --------------------------------
+            if( menuChoice == 1 ){  // 1. 입력한 데이터가 '1' 이면
                 System.out.println("--------------- 예금 -----------------");
-            }
-            else if( menuChoice == 2 ){
+                // 1. 계좌번호 입력
+                System.out.print(" 계좌번호 입력 : ");
+                int 계좌번호 =scanner.nextInt();
+                // 2. 계좌번호 검증
+                if( account == 계좌번호 ){ // 만약에 계좌번호와 입력받은계좌번호와 같으면
+                    // 3. 금액 입력
+                    System.out.print(" 예금 할 금액 입력 : ");
+                    int 예금액 = scanner.nextInt();
+                    if( 예금액 < 0 ){ // 만약에 입력받은 금액이 0보다 작으면
+                        System.out.println("[경고] 예금할수 없는 금액입니다.");
+                    }else{ // 금액이 0보다 크면 예금처리 하기
+                        money += 예금액;   // 4. 통장 잔고에 입력받은 금액을 누적 더하기
+                        System.out.println("[성공] 해당 계좌번호에 예금 되었습니다. ");
+                    }
+                }else{ // 계좌번호가 다를경우
+                    System.out.println("[경고] 계좌번호가 일치하지 않습니다.");
+                } // else end
+            } // if end
+            // ---------------------------------------------------------------------------
+            else if( menuChoice == 2 ){ // 2. 입력한 데이터 '2' 이면
                 System.out.println("--------------- 출금 -----------------");
             }
-            else if( menuChoice == 3 ){
+            else if( menuChoice == 3 ){// 3. 입력한 데이터 '3' 이면
                 System.out.println("--------------- 잔고 -----------------");
             }
-            else if( menuChoice == 4 ){
+            else if( menuChoice == 4 ){ // 4. 입력한 데이터 '4' 이면
                 System.out.println("--------------- 종료 -----------------");
+                break; // -- whlie문 종료
             }
         } // while end
     } // main end
